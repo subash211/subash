006ef9f1b28ea5039be7d329cdd1d69a2a2a1210
subash211 <subash060295@gmail.com>
2023-05-12 05:10:45 +0000

    simple ec2

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
-resource "aws_instance" "Jenkins-started" {
- ami           = var.ami 
- instance_type = var.instance_type
-
- tags = {
-   Name = var.name_tag,
- }
+provider "aws" {
+    region = "ap-south-1"  
 }
 
+resource "aws_instance" "subash" {
+  ami           = "ami-02eb7a4783e7e9317"
+  instance_type = "t2.micro"
+  tags = {
+      Name = "subash"
+  }
+}
