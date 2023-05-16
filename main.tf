5ba24101b4c672029687d8f7dd3b2267bba65ffe
subash211 <131344513+subash211@users.noreply.github.com>
2023-05-16 20:11:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
-provider "aws" {
-    region = "ap-south-1"  
-}
+variable "aws_region" { 
+	default = "ap-south-1" 
+ } 
+variable "aws_secret_key" {} 
+variable "aws_access_key" {} 
+
+provider "aws" { 
+	region = var.aws_region 
+    access_key = var.aws_access_key 
+    secret_key = var.aws_secret_key 
+} 
 
 resource "aws_instance" "subash" {
   ami           = "ami-02eb7a4783e7e9317"
