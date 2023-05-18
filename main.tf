c87206cc8736a84497c764c29a14380701987240
subash211 <subash060295@gmail.com>
2023-05-18 03:59:59 +0000

    simple ec2

--- main.tf
+++ main.tf
@@ -1,15 +1,11 @@
-variable "aws_region" { 
-	default = "ap-south-1" 
- } 
-provider "aws" { 
-	region = "ap-south-1"
-    access_key = "AKIA3RJVOYKC4ZEMVPMQ"
-    secret_key = "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"
-} 
+provider "aws" {
+    region = "ap-south-1"  
+}
+
 resource "aws_instance" "subash" {
-  ami           = "ami-02eb7a4783e7e9317"
+  ami           = "ami-02eb7a4783e7e9317" 
   instance_type = "t2.micro"
   tags = {
-      Name = "subash"
+      Name = "TF-subash"
   }
 }
